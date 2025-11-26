fa6ae9117ee5d8e7f868b85247ab4065d6b2ecf5
Simeon Vladkov <80125788+snVladkov@users.noreply.github.com>
2025-11-26 14:38:02 +0200

    Add status.php for server uptime and response

---
+++ status.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+
+$uptime = null;
+if (is_readable('/proc/uptime')) {
+    $parts = explode(' ', trim(file_get_contents('/proc/uptime')));
+    $uptime = floatval($parts[0]);
+}
+
+$response = [
+    'status' => 'ok',
+    'time' => date('c'),
+    'server_uptime_seconds' => $uptime
+];
+
+http_response_code(200);
+echo json_encode($response);
+exit;
